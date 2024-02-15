0cf442706786eaad4ace8fd85a3e342fb400024c
nichtmehr99 <nichtmehr99@gmail.com>
2024-02-15 14:59:19 +0100

    rectangle calculate done

--- scripts/rectangle-calculator.js
+++ scripts/rectangle-calculator.js
@@ -1,3 +1,15 @@
 function calculateRectangleArea(){
-    console.log('inside function')
+    const rectangleWidthInput = document.getElementById('rectangle-width')
+    const rectangleWidthText = rectangleWidthInput.value
+    const width = parseFloat(rectangleWidthText)
+
+    const rectangleLengthInput = document.getElementById('rectangle-length')
+    const rectangleLengthText = rectangleLengthInput.value
+    const length = parseFloat(rectangleLengthText)
+
+    const area = width * length
+
+    const rectangleAreaSpan = document.getElementById('area-text')
+    rectangleAreaSpan.innerText = area
+    console.log('the rectangle area is : ', area)
 }
